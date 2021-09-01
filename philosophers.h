d39d4e6e9f0c8504c5f92ce3020d65b0d418c3d9
Noufel AMMARI <nammari@e2r7p6.42.fr>
2021-09-01 14:16:23 +0200

    first commit

---
+++ philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nammari <nammari@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 14:12:07 by nammari           #+#    #+#             */
+/*   Updated: 2021/09/01 14:14:31 by nammari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+
+# define PHILOSOPHERS_H
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
