703fe3c298e477927e99c989140914121e5d062b
Noufel AMMARI <nammari@e2r1p1.clusters.42paris.fr>
2021-12-08 16:37:40 +0100

    Instructions on next function to create

--- srcs/parsing/parse_input.c
+++ srcs/parsing/parse_input.c
@@ -6,23 +6,33 @@
 /*   By: nammari <nammari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 11:39:40 by nammari           #+#    #+#             */
-/*   Updated: 2021/12/08 16:30:42 by nammari          ###   ########.fr       */
+/*   Updated: 2021/12/08 16:37:22 by nammari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+int	get_nb_of_philosophers(char **str, t_simulation_data *data)
+{
+	//check if is num
+	//check if unsigned long (strlen)
+	//get_number_using Atolu (atol unsigned)
+	//put_number_in_data
+	//return Success
+}
+
 int	parse_input(int argc, char **argv, t_simulation_data *data)
 {
 	int	i;
 
-	i = 0;
 	if (argc == 1)
 		return (ERROR);
 	if (!(argc == 5 || argc == 6) || argv == NULL || *argv == NULL)
 		return (error_message(PROMPT_USER_INPUT));
+	
+	if (get_nb_of_philosophers(argv, data) == ERROR)
+		return (error_message(PROMPT_USER_INPUT));
 	data->has_nb_time_to_eat = true;
-	data->nb_of_philo = 5;
 	data->nb_time_to_eat = 1;
 	data->starting_time = 10;
 	data->time_to_die = 20;
