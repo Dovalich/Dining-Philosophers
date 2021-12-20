52963499210315062c8d739204b3f4c81372e222
Noufel AMMARI <noufel.ammari@hotmail.com>
2021-12-20 13:46:40 +0100

    understood the functionning of mutex

--- srcs/simulation/ft_usleep.c
+++ srcs/simulation/ft_usleep.c
@@ -6,22 +6,22 @@
 /*   By: nammari <nammari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 14:04:34 by nammari           #+#    #+#             */
-/*   Updated: 2021/12/08 14:04:43 by nammari          ###   ########.fr       */
+/*   Updated: 2021/12/20 12:35:36 by nammari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-// void	ft_usleep(int sleep_time)
-// {
-// 	int	i;
+void	ft_usleep(int sleep_time)
+{
+	int	i;
 
-// 	i = 0;
-// 	while (i < sleep_time)
-// 	{
-// 		usleep(1);
-// 		++i;
-// 	}
-// }
+	i = 0;
+	while (i < sleep_time)
+	{
+		usleep(1);
+		++i;
+	}
+}
 
 // while current_time != the time I want -> ft_usleep (~ 1 micro);
--- srcs/simulation/launch_simulation.c
+++ srcs/simulation/launch_simulation.c
@@ -6,48 +6,52 @@
 /*   By: nammari <nammari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 11:39:58 by nammari           #+#    #+#             */
-/*   Updated: 2021/12/17 16:59:28 by nammari          ###   ########.fr       */
+/*   Updated: 2021/12/20 13:28:27 by nammari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-
-void	get_forks(t_philo *philo)
-{
-	if (philo->left_philo->fork_on_table)
-	{
-		pthread_mutex_lock(&philo->left_philo->mutex);
-		printf("[]%lu ms Philosopher nb %lu has taken left fork\n",philo->data->curr_time, philo->philo_nb);
-		philo->left_philo->fork_on_table = false;
-	}
-	if (philo->right_philo->fork_on_table)
-	{
-		pthread_mutex_lock(&philo->right_philo->mutex);
-		printf("[]%lu ms Philosopher nb %lu has taken right fork\n",philo->data->curr_time, philo->philo_nb);
-		philo->right_philo->fork_on_table = false;
-	}
-}
-
 void	philo_eat(t_philo *philo, t_simulation_data *data)
 {
 	unsigned long	time_to_eat;
 
-	
+	(void)philo;
 	time_to_eat = data->curr_time;
-	while (data->curr_time < time_to_eat + data->time_to_eat)
-	{
-		ft_usleep(1);
-	}
-	
+	usleep(10000);
+	return ;
+	// while (data->curr_time < time_to_eat + data->time_to_eat)
+	// {
+	// 	usleep(100);
+	// }
 }
+
+void	get_forks(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->left_philo->mutex);
+	printf("%03lu ms Philosopher [%lu] has taken left fork\n",philo->data->curr_time, philo->philo_nb);
+	philo->left_philo->fork_on_table = false;
+
+	pthread_mutex_lock(&philo->right_philo->mutex);
+	printf("%03lu ms Philosopher [%lu] has taken right fork\n",philo->data->curr_time, philo->philo_nb);
+	philo->right_philo->fork_on_table = false;
+
+	philo_eat(philo, philo->data);
+	philo->right_philo->fork_on_table = true;
+	printf("%03lu ms Philosopher [%lu] has put right fork down\n",philo->data->curr_time, philo->philo_nb);
+	pthread_mutex_unlock(&philo->right_philo->mutex);
+	philo->left_philo->fork_on_table = true;
+	printf("%03lu ms Philosopher [%lu] has put left fork down \n",philo->data->curr_time, philo->philo_nb);
+	pthread_mutex_unlock(&philo->left_philo->mutex);
+}
+
 
 void	*philo_thread(void *philo)
 {
 	t_philo *ptr;
 	
 	ptr = (t_philo *)philo;
-	if (ptr->data->nb_of_philo % 2 == 0 && ptr->philo_nb % 2 != 0)
-		usleep(5);
+	if (ptr->philo_nb % 2 != 0 && ptr->data->nb_of_philo % 2 == 0)
+		usleep(5000);
 	get_forks(ptr);
 	return (NULL);
 }
@@ -73,7 +77,7 @@ void	*data_thread(void *data)
 	d = data;
 	while (all_philos_alive(d->philo_lst))
 	{
-		update_time(d);
+		continue;
 	}
 	return (NULL);
 }
@@ -84,7 +88,7 @@ int	start_simulation(t_simulation_data *data, t_philo *head)
 	int			i;
 
 	i = 0;
-	pthread_create(&data->thread, NULL, &data_thread, data);
+	// pthread_create(&data->thread, NULL, &data_thread, data);
 	while ((unsigned long)i < data->nb_of_philo)
 	{
 		if (pthread_create(&head->thread, NULL, &philo_thread, (void *)head) != 0)
@@ -98,12 +102,13 @@ int	start_simulation(t_simulation_data *data, t_philo *head)
 		if (head->right_philo != NULL)
 			head = head->right_philo;
 	}
-	pthread_join(data->thread, NULL);
+	// pthread_join(data->thread, NULL);
 	while ((unsigned long)i > 0)
 	{
 		pthread_join(head->thread, NULL);
 		head = head->left_philo;
 		--i;
+		printf("joinin\n");
 	}
 	return (SUCCESS);
 }
