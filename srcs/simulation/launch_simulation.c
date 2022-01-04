a619319f104cc950d3c4957c5235b61fb40c99cd
noufel <noufel.ammari@hotmail.com>
2022-01-04 04:34:37 +0100

    philo dies between 12k and 100k ms with 4 410 200 200

--- srcs/simulation/launch_simulation.c
+++ srcs/simulation/launch_simulation.c
@@ -6,7 +6,7 @@
 /*   By: noufel <noufel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 11:39:58 by nammari           #+#    #+#             */
-/*   Updated: 2022/01/04 01:36:58 by noufel           ###   ########.fr       */
+/*   Updated: 2022/01/04 03:57:39 by noufel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ int	start_simulation(t_simulation_data *data, t_philo *head)
 	ptr = head->right_philo;
 	if (start_even_group(ptr, data->nb_of_philo))
 		return (ERROR);
-	usleep(100);
+	// usleep(1000);
 	if (start_odd_group(head, data->nb_of_philo))
 		return (ERROR);
 	if (pthread_create(&data->thread, NULL, &data_thread, data) == -1)
@@ -83,7 +83,6 @@ int	terminate_simulation(t_simulation_data *data, t_philo *philo)
 		if (philo->left_philo)
 			philo = philo->left_philo;
 		--i;
-		printf("joining\n");
 	}
 	return (SUCCESS);
 }
--- srcs/simulation/philo_thread.c
+++ srcs/simulation/philo_thread.c
@@ -6,7 +6,7 @@
 /*   By: noufel <noufel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 15:13:19 by nammari           #+#    #+#             */
-/*   Updated: 2022/01/04 01:50:38 by noufel           ###   ########.fr       */
+/*   Updated: 2022/01/04 04:27:36 by noufel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,68 @@ bool	is_dead(t_philo *philo, t_simulation_data *data)
 	return (false);
 }
 
+unsigned int	get_hungriest_philo(t_philo *philo)
+{
+	u_timestamp		farthest_last_ate;
+	unsigned int	philo_nb;
+	t_philo			*ptr;
+
+	ptr = philo;
+	farthest_last_ate = philo->last_ate_at;
+	philo_nb = philo->id;
+	while (1)
+	{
+		ptr = ptr->right_philo->right_philo;
+		if (ptr == philo)
+			break ;
+		if (farthest_last_ate > ptr->last_ate_at)
+		{
+			farthest_last_ate = ptr->last_ate_at;
+			philo_nb = ptr->id;
+		}
+	}
+	return (philo_nb);
+}
+
 void	start_eating(t_philo *philo, t_simulation_data *data)
 {
-	pthread_mutex_lock(&philo->fork);
-	pthread_mutex_lock(&philo->right_philo->fork);
-	if (is_dead(philo, data))
+	if (philo->last_ate_at && philo->id != get_hungriest_philo(philo))
+		usleep(400);
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(&philo->right_philo->fork);
+		pthread_mutex_lock(&philo->fork);
+	}
+	else
+	{
+		pthread_mutex_lock(&philo->fork);
+		pthread_mutex_lock(&philo->right_philo->fork);	
+	}
+	if (philo->data->is_dead)
+	{
+		pthread_mutex_unlock(&philo->right_philo->fork);
+		pthread_mutex_unlock(&philo->fork);
 		return ;
+	}
 	print_status(TOOK_FORKS, philo);
 	philo->last_ate_at = data->curr_time;
 	print_status(EATING, philo);
-	while (get_time() - philo->last_ate_at < data->time_to_eat)
+	while (1)
 	{
-		usleep(50);
+		if (get_time() - philo->last_ate_at >= data->time_to_eat)
+			break ;
+		usleep(200);
 	}
-	usleep(100);
-	pthread_mutex_unlock(&philo->right_philo->fork);
-	pthread_mutex_unlock(&philo->fork);
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_unlock(&philo->right_philo->fork);
+		pthread_mutex_unlock(&philo->fork);
+	}
+	else
+	{
+		pthread_mutex_unlock(&philo->fork);
+		pthread_mutex_unlock(&philo->right_philo->fork);
+	}
 }
 
 void	start_sleeping(t_philo *philo, t_simulation_data *data)
@@ -48,14 +94,14 @@ void	start_sleeping(t_philo *philo, t_simulation_data *data)
 			break ;
 		else if (data->curr_time - philo->last_ate_at >= data->time_to_die)
 			break ;
-		usleep(100);
+		usleep(200);
 	}
 }
 
 void	start_thinking(t_philo *philo)
 {
 	print_status(THINKING, philo);
-	usleep(50);
+	usleep(100);
 }
 
 void	*philo_thread(void *philosopher)
@@ -71,10 +117,10 @@ void	*philo_thread(void *philosopher)
 	while (!philo->data->is_dead)
 	{
 		start_eating(philo, philo->data);
-		if (is_dead(philo, philo->data))
+		if (philo->data->is_dead)
 			break ;
 		start_sleeping(philo, philo->data);
-		if (is_dead(philo, philo->data))
+		if (philo->data->is_dead)
 			break ;
 		start_thinking(philo);
 	}
